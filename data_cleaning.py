b04c707da480f2372ba110774ffcaa8b5ff7fe09
Mehtab Sandhu <mehtabsandhu1210@gmail.com>
2025-02-10 15:04:55 -0600

    ohlcv bars are now generated in a separate flat file located in the ohlcv_flat_files directory

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime, timedelta
 import multiprocessing
 from itertools import chain
-import tkinter as tk
+from functools import partial
 
 
 def load_data(directory):
@@ -52,7 +52,7 @@ def clean_data(data_list):
       data_list[item_index][1] = -data_list[item_index][1]
 
     # If the following item is an outlier, likely due to a misplaced decimal point, multiply by 10 to correct.
-    elif data_list[item_index][1] < 300:
+    if data_list[item_index][1] < 300:
       data_list[item_index][1] = data_list[item_index][1] * 10
 
   return data_list
@@ -140,11 +140,66 @@ def generate_timeframe_intervals(time_intervals, timeframes):
   current_timeframe = timeframes['start']
 
   while current_timeframe < timeframes['end']:
-    timeframe_interval_list.append(current_timeframe)
+    list_to_add = [current_timeframe]
     current_timeframe = current_timeframe + timedelta(seconds=time_intervals['s'], minutes=time_intervals['m'], hours=time_intervals['h'], days=time_intervals['d'])
+    list_to_add.append(current_timeframe)
+    timeframe_interval_list.append(list_to_add)
   
+  timeframe_interval_list[-1][1] = timeframes['end']
+
   return timeframe_interval_list
 
+
+def generate_ohlcv(list_of_data, timeframe_interval):
+  start_index = 0
+
+  left = 0
+  right = len(list_of_data) - 1
+  mid = 0
+  foundMid = False
+
+  while left <= right:
+    mid = left + (right - left) // 2
+
+    if list_of_data[mid][0] == timeframe_interval[0]:
+      start_index = mid
+      foundMid = True
+    elif list_of_data[mid][0] < timeframe_interval[0]:
+      left = mid + 1
+    else:
+      right = mid - 1
+  
+  if not foundMid:
+    start_index = mid
+
+  ohlcv = [
+    timeframe_interval[0],   # Timestamp
+    list_of_data[start_index][1],  # Open
+    list_of_data[start_index][1],  # High
+    list_of_data[start_index][1],  # Low
+    None, # Close
+    0,    # Volume
+  ]
+
+  while start_index < len(list_of_data) and list_of_data[start_index][0] < timeframe_interval[1]:
+    ohlcv[2] = max(ohlcv[1], list_of_data[start_index][1]) # High
+    ohlcv[3] = min(ohlcv[2], list_of_data[start_index][1]) # Low
+    ohlcv[5] += list_of_data[start_index][2]               # Volume
+    start_index += 1
+  
+  ohlcv[4] = list_of_data[start_index - 1][1] # Close
+
+  return ohlcv
+
+
+def generate_flat_file(ohlcv_list, timeframe):
+  os.makedirs('ohlcv_flat_files', exist_ok=True)
+  with open(os.path.join('ohlcv_flat_files', 'ohlcv_{start}_{end}.csv'.format(start=str(timeframe['start'])[:10], end=str(timeframe['end'])[:10])), 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    for ohlcv in ohlcv_list:
+      writer.writerow(ohlcv)
+  
 
 def main():
   # Creates a pool of processes to run the script in parallel (one process per CPU core).
@@ -162,11 +217,36 @@ def main():
   # Flattens the list of lists into a single list.
   data_list = list(chain(*data_list))
 
-  # time_intervals = grab_time_interval()
-  # timeframe = grab_timeframe()
+  # Grabs the time interval and timeframe from the user.
+  time_intervals = grab_time_interval()
+  timeframe = grab_timeframe()
 
-  # print(generate_timeframe_intervals(time_intervals, timeframe))
+  # Generates the timeframe intervals to create the OHLCV records.
+  timeframe_intervals = generate_timeframe_intervals(time_intervals, timeframe)
 
+  # print(timeframe['start'].year + timeframe['start'].month + timeframe['start'].day)
+  # print(str(timeframe['start'])[:11] + str(timeframe['end'])[:11])
+
+  # Creates a pool of processes to run the script in parallel (one process per CPU core).
+  ohlcv_pool = multiprocessing.Pool(processes=os.cpu_count())
+
+  # Creates a partial function to pass the data_list to the generate_ohlcv function.
+  # This is required to use ohlcv_pool.map() afterwards
+  generate_ohlcv_partial = partial(generate_ohlcv, data_list)
+
+  # Generates the OHLCV records for each timeframe interval.
+  ohlcv_list = ohlcv_pool.map(generate_ohlcv_partial, timeframe_intervals)
+
+  # Closes the pool of processes, and waits for them to finish.
+  ohlcv_pool.close()
+  ohlcv_pool.join()
+
+  # Prints the OHLCV records.
+  for ohlcv in ohlcv_list:
+    print(ohlcv)
+  
+  # Creates the csv flat file containing the ohlcv bars
+  generate_flat_file(ohlcv_list, timeframe)
   
 
 if __name__ == '__main__':
