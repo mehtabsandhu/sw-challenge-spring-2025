929e6ab61d76d9022f8f26afe29f6aabb17ea291
Mehtab Sandhu <mehtabsandhu1210@gmail.com>
2025-02-10 04:22:08 -0600

    Added functions to grab user input for the time intervals, and the timeframes for ohlcv bars

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,9 +1,10 @@
 # These are the 5 libraries that are used in this script.
 import csv
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import multiprocessing
 from itertools import chain
+import tkinter as tk
 
 
 def load_data(directory):
@@ -74,25 +75,99 @@ def remove_duplicates(data_list):
   return data_list
 
 
-def main():
+def grab_time_interval():
+  valid_input = False
+  smhd = {}
+  while not valid_input:
+    time_interval = input("Enter the desired time interval: ").lower()
+    char_index = 0
+    current_index = 0
+    smhd = {}
+    for char in time_interval:
+      if char not in 'smhd0123456789':
+        print("Invalid input. Please enter a valid time interval.")
+        valid_input = False
+        break
+      if char in 'smhd':
+        if current_index == 0 or current_index == char_index:
+          print("Invalid input. Please enter a valid time interval.")
+          valid_input = False
+          break
+        valid_input = True
+        smhd[char] = int(time_interval[char_index:current_index])
+        char_index = current_index + 1
+      current_index += 1
+  if 's' not in smhd:
+    smhd['s'] = 0
+  if 'm' not in smhd:
+    smhd['m'] = 0
+  if 'h' not in smhd:
+    smhd['h'] = 0
+  if 'd' not in smhd:
+    smhd['d'] = 0
+  return smhd
 
+
+def grab_timeframe():
+  valid_start = False
+  valid_end = False
+  timeframe = {}
+
+  while not valid_start:
+    start_date = input("Enter the start datetime in the following format (YYYY-MM-DD): ")
+    try:
+      start_date = datetime.strptime(start_date, '%Y-%m-%d')
+      valid_start = True
+      timeframe['start'] = start_date
+    except ValueError:
+      print("Invalid input. Please enter a valid date.")
+  
+  while not valid_end:
+    end_date = input("Enter the end datetime in the following format (YYYY-MM-DD): ")
+    try:
+      end_date = datetime.strptime(end_date, '%Y-%m-%d')
+      valid_end = True
+      timeframe['end'] = end_date
+    except ValueError:
+      print("Invalid input. Please enter a valid date.")
+  
+  return timeframe
+
+
+def generate_timeframe_intervals(time_intervals, timeframes):
+  timeframe_interval_list = []
+
+  current_timeframe = timeframes['start']
+
+  while current_timeframe < timeframes['end']:
+    timeframe_interval_list.append(current_timeframe)
+    current_timeframe = current_timeframe + timedelta(seconds=time_intervals['s'], minutes=time_intervals['m'], hours=time_intervals['h'], days=time_intervals['d'])
+  
+  return timeframe_interval_list
+
+
+def main():
   # Creates a pool of processes to run the script in parallel (one process per CPU core).
-  pool = multiprocessing.Pool(processes=os.cpu_count())
+  data_loading_pool = multiprocessing.Pool(processes=os.cpu_count())
 
   # Loads the data from the files, cleans it, and removes duplicates.
-  data_list = pool.map(load_data, os.listdir('data'))
-  data_list = pool.map(remove_duplicates, data_list)
-  data_list = pool.map(clean_data, data_list)
+  data_list = data_loading_pool.map(load_data, os.listdir('data'))
+  data_list = data_loading_pool.map(remove_duplicates, data_list)
+  data_list = data_loading_pool.map(clean_data, data_list)
 
   # Closes the pool of processes, and waits for them to finish.
-  pool.close()
-  pool.join()
+  data_loading_pool.close()
+  data_loading_pool.join()
 
   # Flattens the list of lists into a single list.
   data_list = list(chain(*data_list))
 
-  print(len(data_list))
+  # time_intervals = grab_time_interval()
+  # timeframe = grab_timeframe()
 
+  # print(generate_timeframe_intervals(time_intervals, timeframe))
+
+  
 
 if __name__ == '__main__':
   main()
